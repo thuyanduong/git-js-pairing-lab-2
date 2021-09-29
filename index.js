fcad2aecdb6c2cc3898c61bc1816c231cbfc3d17
Mark Rodriguez <markrodriguezaa@gmail.com>
2021-09-29 18:30:32 +0000

    complte problem 2

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 function OneHundredToFive(){
     for (let i = 100; i >= 5; i--){
-        console.log(i)
+        //console.log(i)
     }
 }
 
-OneHundredToFive()
+//OneHundredToFive()
+
+function absoluteValue(num){
+    if (typeof num === "number"){
+        return Math.abs(num)
+    }else{
+        return null
+    }
+}
+console.log(absoluteValue("string"));
